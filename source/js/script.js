71668dd151568283982ed6a8aa5958dc4f5286ce
slyvka-tanya <77440028+slyvka-tanya@users.noreply.github.com>
2021-05-19 09:50:20 +0300

    исправление защит

--- source/js/script.js
+++ source/js/script.js
@@ -1,11 +1,11 @@
 const mainNavToggle = document.querySelector(".main-header__toggle");
 const mainNav = document.querySelector(".main-nav");
 
-mainNav.classList.add("modal-close");
+mainNav.classList.add("main-nav--close");
 mainNavToggle.classList.add("main-header__toggle--close");
 
 mainNavToggle.addEventListener("click", function () {
-  mainNav.classList.toggle("modal-show");
+  mainNav.classList.toggle("main-nav--open");
   mainNavToggle.classList.toggle("main-header__toggle--open");
   mainNavToggle.classList.toggle("main-header__toggle--close");
 });
